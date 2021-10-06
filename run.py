714e8c768dd525aa4fd4f372bc39e54f1faf246e
autoblack <cclauss@users.noreply.github.com>
2021-10-06 07:01:22 +0000

    fixup: Format Python code with Black

--- run.py
+++ run.py
@@ -6,7 +6,9 @@ from measures.maintainability import MaintainabilityTest
 from measures.currency import CurrencyTest
 
 logger = logging.getLogger("measures")
-log_format = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+log_format = logging.Formatter(
+    "%(asctime)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
 log_handler = logging.StreamHandler()
 log_handler.setFormatter(log_format)
 logger.addHandler(log_handler)
@@ -18,7 +20,7 @@ results.append(MaintainabilityTest().test())
 results.append(CurrencyTest().test())
 
 if not all(results):
-    logger.error('FAILURE')
+    logger.error("FAILURE")
     sys.exit(1)
 
-logger.info('PASS')
+logger.info("PASS")
